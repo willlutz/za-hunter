22f879517fd276848d6ded9d2b5db24b048d6fb0
will lutz <wlutz@bsd220.org>
2024-02-27 09:28:34 -0600

    Initial Commit

---
+++ za hunter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  za hunter
+//
+//  Created by will lutz on 2/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ za hunter/za_hunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  za_hunterApp.swift
+//  za hunter
+//
+//  Created by will lutz on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct za_hunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
